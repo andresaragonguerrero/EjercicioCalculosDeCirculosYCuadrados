be8df407e9f6c81963a407876e302209dea6f30f
andresaragonguerrero <andresaragonguerrero@gmail.com>
2025-11-05 12:45:44 +0100

    Se eliminó el ejercicio de las figuras para ampliarlo

--- script.ts
+++ script.ts
@@ -11,56 +11,6 @@ class Alumno {
 
 const alumno = new Alumno("Antonio", 20, true);
 
-// hayar superficie y perímetro
-class Cuadrado {
-    x: number;
-    y: number;
-    longLado: number;
-    constructor(x: number, y: number, longLado: number) {
-        this.x = x;
-        this.y = y;
-        this.longLado = longLado;
-    }
-    mostrarAreaCuadrado(): number {
-        return this.x * this.y;
-    }
-    mostrarPerimetroCuadrado(): number {
-        return this.x * 4;
-    }
-}
-
-const cuadrado = new Cuadrado(5, 6, 10);
-const cuadrado2 = new Cuadrado(5, 6, 10);
-console.log(`Área del cuadrado 1: ` + cuadrado.mostrarAreaCuadrado());
-console.log(`Perímetro del cuadrado 1: ` + cuadrado.mostrarPerimetroCuadrado());
-console.log(`Área del cuadrado 2: ` + cuadrado2.mostrarAreaCuadrado());
-console.log(`Perímetro del cuadrado 2: ` + cuadrado2.mostrarPerimetroCuadrado());
-
-// hayar superficie y perímetro
-class Circulo {
-    x: number;
-    y: number;
-    longRadio: number;
-    constructor(x: number, y: number, longRadio: number) {
-        this.x = x;
-        this.y = y;
-        this.longRadio = longRadio;
-    }
-    mostrarAreaCirculo(): number {
-        return Math.PI * this.longRadio * this.longRadio;
-    }
-    mostrarPerimetroCirculo(): number {
-        return 2 * Math.PI * this.longRadio;
-    }
-}
-
-const circulo = new Circulo(2, 2, 4);
-const circulo2 = new Circulo(2, 2, 4);
-console.log(`Área del círculo 1: ` + circulo.mostrarAreaCirculo());
-console.log(`Perímetro del círculo 1: ` + circulo.mostrarPerimetroCirculo());
-console.log(`Área del círculo 2: ` + circulo2.mostrarAreaCirculo());
-console.log(`Perímetro del círculo 1: ` + circulo2.mostrarPerimetroCirculo());
-
 // calcular IMC corporal: IMC = peso (kg) / altura² (m²)
 // peso inferior: menos de 18.5
 // peso normal: entre 18.5 y 24.9
@@ -96,3 +46,4 @@ class Persona {
 
 const antonio = new Persona("Antonio", 72, 1.74);
 antonio.mostrarMensaje();
+
